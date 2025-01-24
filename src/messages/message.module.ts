5ee732cfab54bcda1305e3b8387864ac8d3e27da
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-01-24 08:55:36 -0300

    refactor: chang from pt-br to en, use findOne in update, add Person

--- src/messages/message.module.ts
+++ src/messages/message.module.ts
@@ -3,10 +3,14 @@ import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message';
+import { PersonModule } from 'src/person/person.module';
 
 @Module({
   controllers: [MessageController],
   providers: [MessageService],
-  imports: [TypeOrmModule.forFeature([Message])]
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    PersonModule
+  ]
 })
 export class MessageModule {}
--- src/messages/message.service.ts
+++ src/messages/message.service.ts
@@ -1,32 +1,85 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConsoleLogger, Injectable, NotFoundException } from '@nestjs/common';
 import { Message } from './entities/message';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, TreeRepository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { PersonService } from 'src/person/person.service';
+import { Person } from '../person/entities/person.entity';
 
 @Injectable()
 export class MessageService {
   constructor(
     @InjectRepository(Message)
     private messageRepository: Repository<Message>,
+    private readonly personService: PersonService,
   ) {}
 
-  addMessage(newMessage: CreateMessageDto): Promise<Message> {
+  async addMessage(
+    createMessageDto: CreateMessageDto,
+  ): Promise<ResponseMessageDTO> {
+    const { fromId, toId, text } = createMessageDto;
+
+    const userFromDb = await this.personService.findOne(fromId);
+    const userToDb = await this.personService.findOne(toId);
+
+    const newMessage = {
+      text,
+      from: userFromDb,
+      to: userToDb,
+      // isRead: false,
+      // date: new Date()
+    };
 
     const message = this.messageRepository.create(newMessage);
 
-    return this.messageRepository.save(message);
+    await this.messageRepository.save(message);
+
+    return {
+      ...message,
+      from: {
+        id: userFromDb.id,
+      },
+      to: {
+        id: userToDb.id,
+      },
+    };
   }
 
+  //Nos metodos do repositiry da para configurar relacoes e response
   async findAll(): Promise<Message[]> {
-    const messagesDB = await this.messageRepository.find();
+    const messagesDB = await this.messageRepository.find({
+      relations: ['from', 'to'],
+      select: {
+        from: {
+          id: true,
+          name: true,
+        },
+        to: {
+          id: true,
+          name: true,
+        },
+      },
+    });
 
     return messagesDB;
   }
 
   async findOne(id: number): Promise<Message> {
-    const messageDB = await this.messageRepository.findOne({ where: { id } });
+    const messageDB = await this.messageRepository.findOne({
+      where: { id },
+      relations: ['from', 'to'],
+      select: {
+        from: {
+          id: true,
+          email: true,
+        },
+        to: {
+          id: true,
+          name: true,
+        },
+      },
+    });
     if (!messageDB) {
       // throw new Error('Erro do servidor'); VAI GERAR UM 500
       // throw new HttpException(`Usuario de ID ${id} nao encontrado`, HttpStatus.NOT_FOUND); Forma Geral
@@ -38,11 +91,25 @@ export class MessageService {
   }
 
   async update(id: number, updateMessage: UpdateMessageDto): Promise<Message> {
-    //Regra: Apenas essas propriedades podem ser alteradas
-    const partialUpdateMessage = {
-      lido: updateMessage?.isRead,
-      texto: updateMessage?.text
-    }
+    const messageDb = await this.findOne(id);
+
+    // const partialUpdateMessage = {
+    //   isRead: updateMessage?.isRead,
+    //   text: updateMessage?.text,
+    // }
+
+    const isRead = updateMessage.isRead
+      ? updateMessage.isRead
+      : messageDb.isRead;
+    const text = updateMessage.text 
+      ? updateMessage.text 
+      : messageDb.text;
+
+    const updatedMessage = {
+      ...messageDb,
+      isRead,
+      text
+    };
 
     // const updated = this.recadoRepository.update(id, updateRecado); Apenas atualiza
 
@@ -51,22 +118,23 @@ export class MessageService {
     //   ...updateRecado,
     // })
 
-    const updatedMessageInstace = await this.messageRepository.preload({
-      id, // Garante que estamos mesclando com a entidade existente
-      ...partialUpdateMessage // Atualiza os campos necessários
-    });
+    //preload nao tras a relacoes, por isso vou usar findOne
+    // const updatedMessageInstace = await this.messageRepository.preload({
+    //   id, // Garante que estamos mesclando com a entidade existente
+    //   ...partialUpdateMessage, // Atualiza os campos necessários
+    // });
 
-    if(!updatedMessageInstace) {
-      throw new NotFoundException(`Recado para ID ${id} nao encontrado`);
-    }
+    // if (!updatedMessageInstace) {
+    //   throw new NotFoundException(`Recado para ID ${id} nao encontrado`);
+    // }
 
-    return this.messageRepository.save(updatedMessageInstace);
+    return this.messageRepository.save(updatedMessage);
   }
 
   async remove(id: number): Promise<{ message: string }> {
     await this.findOne(id);
 
     await this.messageRepository.delete(id);
-    return {message: `Recado de ID ${id}, Deletado`};
+    return { message: `Recado de ID ${id}, Deletado` };
   }
 }
