b743da0e3d79beebf4475e0a82bcf976de903c5e
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-01-25 08:19:08 -0300

    feat: add custom pipe

---
+++ src/common/pipes/parse-int-id.pipes.ts
@@ -0,0 +1,37 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+
+export class ParseIntIdPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (metadata.type !== 'param' || metadata.data !== 'id') {
+      return value;
+    }
+
+    console.log(value);
+    console.log(metadata);
+
+    const parsedValue = Number(value);
+
+    if (isNaN(parsedValue)) {
+      throw new BadRequestException('ParseIntIdPipe expects a numeric string');
+    }
+
+    if (parsedValue < 0) {
+      throw new BadRequestException('ParseIntIdPipe expects a number greater than zero')
+    }
+
+    return parsedValue;
+  }
+}
+
+// Criando proprio Pipe: implemtar a Interface PipeTransform
+// Pdoe ser usado de forma Global no app.ValidationPipes
+/*    app.useGlobalPipes(
+          new ValidationPipe(
+            {},
+            new ParseIntPipe(),
+            )
+) */   
--- src/messages/message.controller.ts
+++ src/messages/message.controller.ts
@@ -10,13 +10,16 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
 } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { Message } from './entities/message';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { PaginationDTO } from 'src/common/dto/pagination.dto';
+import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipes';
 
+@UsePipes(ParseIntIdPipe)
 @Controller('recados')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
@@ -29,7 +32,7 @@ export class MessageController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number): Promise<Message> {
+  findOne(@Param('id') id: number): Promise<Message> {
     return this.messageService.findOne(id);
   }
 
@@ -40,7 +43,7 @@ export class MessageController {
 
   @Patch(':id')
   update(
-    @Param('id', ParseIntPipe) id: number,
+    @Param('id') id: number,
     @Body() recado: UpdateMessageDto,
   ) {
     return this.messageService.update(+id, recado);
@@ -48,7 +51,7 @@ export class MessageController {
 
   // @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: string): Promise<{ message: string }> {
+  remove(@Param('id') id: string): Promise<{ message: string }> {
     return this.messageService.remove(+id);
   }
 }
@@ -58,3 +61,11 @@ export class MessageController {
 
 // Enum de status
 // @HttpCode(HttpStatus.CREATED)
+
+
+// Pipes: de validacao, transformacao e valiacao/transformacao
+//    Global com app.useGlobalPipes(new ValidationPipe({}))
+//    No controller glogal usando, ou no metodo @UsePipes(ParseIntPipe)
+//    Como parametro findOne(@Param('id', ParseIntPipe) id: number)
+//    Como objeto parametro, para usar mais propriendafe
+//        findOne(@Param('id', new ParseIntPipe({})) id: number)
