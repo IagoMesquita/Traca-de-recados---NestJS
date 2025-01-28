9c4c7c407bad904f32801c2c6a28cd453b5c06cd
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-01-27 21:49:48 -0300

    feat: config middleware in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from 'src/person/person.module';
 import { MessageModule } from 'src/messages/message.module';
+import { SimpleMiddleware } from 'src/common/middlewares/simple.middleware';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { MessageModule } from 'src/messages/message.module';
       password: 'iago007',
       database: 'recados_db',
       autoLoadEntities: true, // Carrega entidades sem precisar especifica-las.
-      synchronize: true, // No usar em producao
+      synchronize: true, // No usar em producao 
     }),
     MessageModule,
     PersonModule,
@@ -23,6 +24,14 @@ import { MessageModule } from 'src/messages/message.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SimpleMiddleware).forRoutes({
+      path: '/persons/*', // -> soemente para a persons e qualquer caminho depois
+      // path: '*', -> Pata todas as rotas
+      method: RequestMethod.ALL
+    })
+  }
+}
 
 // npm i --save @nestjs/typeorm typeorm pg
