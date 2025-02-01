eff834ef46d084543c66f0360430ca5797a8b8e4
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-02-01 11:41:11 -0300

    feat: add environments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { PersonModule } from 'src/person/person.module';
 import { MessageModule } from 'src/messages/message.module';
 import { SimpleMiddleware } from 'src/common/middlewares/simple.middleware';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { GlobalExceptionsFilter } from 'src/common/ExceptionsFilter/global-exceptions.filter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // envFilePath: ['env/.env'], Passar mais de um arquivo de variaveis, ou com nome diferente de .env
+      // ignoreEnvFile: true, // Caso as variaveis sejam setadas direto no servidor, como Heroku
+    }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'iago007',
-      database: 'recados_db',
-      autoLoadEntities: true, // Carrega entidades sem precisar especifica-las.
-      synchronize: true, // No usar em producao 
+      type: process.env.DATABASE_TYPE as "postgres",
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Carrega entidades sem precisar especifica-las.
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // No usar em producao 
     }),
     MessageModule,
     PersonModule,
