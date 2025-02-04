b1bff6bc9ef245a1bfe50ff7f5f6cd2bf034de91
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-02-03 21:34:17 -0300

    chore: add module person

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,22 @@
 import { Global, Module } from "@nestjs/common";
 import { HasingServiceProtocol } from "./hasing/hashing.protocol.service";
 import { BcryptService } from "./hasing/bcrypt.service";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Person } from "src/person/entities/person.entity";
 
 @Global()
 @Module({
+  controllers: [AuthController],
   providers: [
+    AuthService,
     {
       provide: HasingServiceProtocol,
       useClass: BcryptService
     }
   ],
+  imports: [TypeOrmModule.forFeature([Person])],
   exports: [HasingServiceProtocol]
 })
 export class AuthModule {}
