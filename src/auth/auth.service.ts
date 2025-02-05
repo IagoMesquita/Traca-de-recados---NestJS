18a9d25feff70bedfe0dd9f8396f2fc59c63578b
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-02-05 20:56:31 -0300

    feat: add jwtConfiguration and jwtService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,38 +1,67 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDto } from './dto/loogin.dto';
 import { HasingServiceProtocol } from './hasing/hashing.protocol.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Person } from 'src/person/entities/person.entity';
 import { Repository } from 'typeorm';
+import jwtConfig from './config/jwt.config';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly authService: HasingServiceProtocol,
     @InjectRepository(Person)
     private readonly personRepository: Repository<Person>,
-    private readonly authService: HasingServiceProtocol,
-  ) {}
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private readonly jwtService: JwtService,
+  ) {
+    console.log(jwtConfiguration);
+  }
 
-  async signIn(loginDto: LoginDto): Promise<any> {
+  async signIn(loginDto: LoginDto): Promise<{message: string, access_token: string }> {
     const { email, password } = loginDto;
 
-    const personDB = await this.personRepository.findOne({ where: { email }});
+    const personDB = await this.personRepository.findOne({ where: { email } });
 
     if (!personDB) {
-      throw new NotFoundException(`Pessoa nao encontrada para o email: ${email}`);
+      throw new NotFoundException(
+        `Pessoa nao encontrada para o email: ${email}`,
+      );
     }
 
-    const isMatch = await this.authService.compare(password, personDB.passwordHash);
-    
+    const isMatch = await this.authService.compare(
+      password,
+      personDB.passwordHash,
+    );
+
     if (!isMatch) {
-      throw new UnauthorizedException('Usuario nao autorizado')
+      throw new UnauthorizedException('Usuario nao autorizado');
     }
 
-  
+    const payload = {
+      sub: personDB.id,
+      username: personDB.name,
+      email: personDB.email,
+    };
+
+    const config = {
+      audience: this.jwtConfiguration.audience,
+      issuer: this.jwtConfiguration.issuer,
+      secret: this.jwtConfiguration.secret,
+      expiresIn: this.jwtConfiguration.jwtTtl,
+    }; 
 
     return {
-      loginDto,
-      message: "usuario logado!"
+      message: 'Login Success!',
+      access_token: await this.jwtService.signAsync(payload, config),
     };
   }
 }
