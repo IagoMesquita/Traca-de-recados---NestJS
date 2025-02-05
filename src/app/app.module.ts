d46d2c08d5df94a8cc047ae94bcf0b3271fff13c
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-02-05 20:54:07 -0300

    feat: add valitation environment with joi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from 'src/person/person.module';
 import { MessageModule } from 'src/messages/message.module';
 import { SimpleMiddleware } from 'src/common/middlewares/simple.middleware';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
-import { GlobalExceptionsFilter } from 'src/common/ExceptionsFilter/global-exceptions.filter';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth/auth.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       // envFilePath: ['env/.env'], Passar mais de um arquivo de variaveis, ou com nome diferente de .env
-      // ignoreEnvFile: true, // Caso as variaveis sejam setadas direto no servidor, como Heroku
+      // ignoreEnvFile: true, // Caso as variaveis sejam setadas direto no servidor, como Heroku,
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.required(),
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.required(),
+        DATABASE_DATABASE: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_AUTOLOADENTITIES: Joi.boolean().default(false), 
+        DATABASE_SYNCHRONIZE: Joi.boolean().default(false) 
+      })
     }),
     TypeOrmModule.forRoot({
       type: process.env.DATABASE_TYPE as "postgres",
@@ -58,3 +67,5 @@ export class AppModule implements NestModule {
 }
 
 // npm i --save @nestjs/typeorm typeorm pg
+// npm i joi
+// npm i -D @types/hapi__join 
