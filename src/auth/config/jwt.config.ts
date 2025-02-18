e373880e5b66749fc88ec76a6a399f4192b15757
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-02-18 07:56:38 -0300

    refactor: add jwtRefreshTtl in config

--- src/auth/config/jwt.config.ts
+++ src/auth/config/jwt.config.ts
@@ -5,6 +5,7 @@ export default registerAs('jwt', () => ({
   audience: process.env.JWT_TOKEN_AUDIENCE,
   issuer: process.env.JWT_TOKEN_ISSUER,
   jwtTtl: Number(process.env.JWT_TTL) || 3600,
+  jwtRefreshTtl: Number(process.env.JWT_REFRESH_TTL) || 86400,
 }));
 
 
