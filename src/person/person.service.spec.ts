aedb0133df8b1ef2c0ff212d1de8ba39b78f242b
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-03-01 13:14:00 -0300

    test: add exception e service Person

--- src/person/person.service.spec.ts
+++ src/person/person.service.spec.ts
@@ -5,6 +5,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HasingServiceProtocol } from 'src/auth/hasing/hashing.protocol.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreatePersonDto } from './dto/create-person.dto';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 
 describe('Person Service', () => {
   let personService: PersonService;
@@ -15,13 +16,12 @@ describe('Person Service', () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       providers: [
         PersonService,
-        { 
-          provide: getRepositoryToken(Person), 
+        {
+          provide: getRepositoryToken(Person),
           useValue: {
             create: jest.fn(),
             save: jest.fn(),
-            
-          } 
+          },
         },
         {
           provide: HasingServiceProtocol,
@@ -48,33 +48,73 @@ describe('Person Service', () => {
 
   describe('create', () => {
     it('must create a new person', async () => {
-      // ARRANGE
       // CreatePersonDto
-      const createPersonDto: CreatePersonDto = {
-        email: "jh@gmail.com",
-        name: "Joao Heleno",
-        password: "010203"
-      };
-
-      const passwordHash = "HASHDESENHA"
       // Que o hashingService tenha o metodo hash
-      jest.spyOn(hashingService, 'toHash').mockResolvedValue(passwordHash);
       // Saber se o hashing service foi chamado com CreatePersonDto
       // Saber se o pesersonRepository.create foi chamado com dados person
       // Sabser se personRepository.save foi chamado com a person criada
       // O retorno final deve ser a nova person criada -> expect
 
+      // ARRANGE
+      const createPersonDto: CreatePersonDto = {
+        email: 'jh@gmail.com',
+        name: 'Joao Heleno',
+        password: '010203',
+      };
+      const passwordHash = 'HASHDESENHA';
+
+      const newPerson = {
+        id: 1,
+        email: 'jh@gmail.com',
+        name: 'Joao Heleno',
+        passwordHash,
+      };
+
+      jest.spyOn(hashingService, 'toHash').mockResolvedValue(passwordHash);
+      jest.spyOn(personRepository, 'create').mockReturnValue(newPerson as any);
+      jest.spyOn(personRepository, 'save').mockResolvedValue(newPerson as any);
 
       //ACT
-      await personService.create(createPersonDto);
+      const response = await personService.create(createPersonDto);
 
       // ASSERT
       expect(hashingService.toHash).toHaveBeenCalledTimes(1);
-      expect(hashingService.toHash).toHaveBeenCalledWith(createPersonDto.password);
+      expect(hashingService.toHash).toHaveBeenCalledWith(
+        createPersonDto.password,
+      );
 
-  
+      expect(personRepository.create).toHaveBeenCalledTimes(1);
+      expect(personRepository.create).toHaveBeenCalledWith({
+        email: 'jh@gmail.com',
+        name: 'Joao Heleno',
+        passwordHash: 'HASHDESENHA',
+      });
+
+      expect(personRepository.save).toHaveBeenCalledTimes(1);
+      expect(personRepository.save).toHaveBeenCalledWith(newPerson);
+      expect(response).toEqual(newPerson);
     });
-  })
+
+    it('should throw ConflictException when email already exists', async () => {
+      jest.spyOn(personRepository, 'save').mockRejectedValue({
+        code: '23505'
+      });
+
+      await expect(personService.create({} as any)).rejects.toThrow(
+        ConflictException
+      );
+    });
+
+    it('should throw Exception for a generic error', async () => {
+      jest.spyOn(personRepository, 'save').mockRejectedValue(
+        new Error('Erro generico')
+      );
+
+      await expect(personRepository.save).rejects.toThrow(
+        new  Error('Erro generico')
+      );
+    });
+  });
 });
 
 describe('Conceito de AAA para testes', () => {
@@ -91,7 +131,6 @@ describe('Conceito de AAA para testes', () => {
 
 // Cannot find module 'src/messages/entities/message' from 'person/entities/person.entity.ts'
 // Necessario configurar no package para encontrar os caminhos.
-
 
 // Sobre jest.fn e jest.spyOn
 // https://chatgpt.com/share/67bdaccb-f944-800a-a3f9-00f8cb4dccae
